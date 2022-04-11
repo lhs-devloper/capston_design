d5db39f41b4e34202b4ecf2667d93cb0efc9f2ab
lhs-devloper <sdsd0114@naver.com>
2022-04-12 01:33:53 +0900

    second commit

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from scrapping import Crawling
 
 if __name__ == "__main__":
+    # 0~100
+    # for i in range(1, 100):
+    #     url = f"https://1xbet.whoscored.com/Players/{91909+i}/History"
+    #     Crawling(url).setting()
+    #     scrapped = Crawling(url)
+    #     scrapped.run()
 
-    for i in range(1):
-        url = f"https://1xbet.whoscored.com/Players/{91909+i}/History"
+    url = "https://1xbet.whoscored.com/Players/91908/History"
+    Crawling(url).setting()
+    scrapped = Crawling(url)
+    scrapped.run()
 
-        scrapped = Crawling(url)
-        scrapped.run()
+    # url = "https://1xbet.whoscored.com/Players/3203/History"
+    # Crawling(url).setting()
+    # scrapped = Crawling(url)
+    # scrapped.run()
+
+    # url = "https://1xbet.whoscored.com/Players/91910/history"
+    # Crawling(url).setting()
+    # scrapped = Crawling(url)
+    # scrapped.run()
