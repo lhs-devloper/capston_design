fd87263519424860b1b6fc56a090febf7fb41fce
lhs-devloper <sdsd0114@naver.com>
2022-06-08 04:53:45 +0900

    complete

--- test.py
+++ test.py
@@ -1,87 +1,198 @@
-from dash import Dash, dcc, html, Input, Output
-import plotly.express as px
+import dash
+from dash.dependencies import Input, Output, State
+import dash_html_components as html
+import dash_daq as daq
 
-import pandas as pd
+app = dash.Dash(__name__)
 
-app = Dash(__name__)
+theme = {
+    "dark": True,
+    "detail": "#007439",
+    "primary": "#00EA64",
+    "secondary": "#6E6E6E",
+}
 
-df = pd.read_csv("https://plotly.github.io/datasets/country_indicators.csv")
+rootLayout = html.Div(
+    [
+        daq.BooleanSwitch(
+            on=True, id="darktheme-daq-booleanswitch", className="dark-theme-control"
+        ),
+        html.Br(),
+        daq.ToggleSwitch(
+            id="darktheme-daq-toggleswitch", className="dark-theme-control"
+        ),
+        html.Br(),
+        daq.ColorPicker(
+            value=dict(hex="#171717"),
+            id="darktheme-daq-colorpicker",
+            className="dark-theme-control",
+        ),
+        html.Br(),
+        daq.Gauge(
+            min=0,
+            max=10,
+            value=6,
+            color=theme["primary"],
+            id="darktheme-daq-gauge",
+            className="dark-theme-control",
+        ),
+        html.Br(),
+        daq.GraduatedBar(
+            value=4,
+            color=theme["primary"],
+            id="darktheme-daq-graduatedbar",
+            className="dark-theme-control",
+        ),
+        html.Br(),
+        daq.Indicator(
+            value=True,
+            color=theme["primary"],
+            id="darktheme-daq-indicator",
+            className="dark-theme-control",
+        ),
+        html.Br(),
+        daq.Knob(
+            min=0,
+            max=10,
+            value=6,
+            id="darktheme-daq-knob",
+            className="dark-theme-control",
+        ),
+        html.Br(),
+        daq.LEDDisplay(
+            value="3.14159",
+            color=theme["primary"],
+            id="darktheme-daq-leddisplay",
+            className="dark-theme-control",
+        ),
+        html.Br(),
+        daq.NumericInput(
+            min=0,
+            max=10,
+            value=4,
+            id="darktheme-daq-numericinput",
+            className="dark-theme-control",
+        ),
+        html.Br(),
+        daq.PowerButton(
+            on=True,
+            color=theme["primary"],
+            id="darktheme-daq-powerbutton",
+            className="dark-theme-control",
+        ),
+        html.Br(),
+        daq.PrecisionInput(
+            precision=4,
+            value=299792458,
+            id="darktheme-daq-precisioninput",
+            className="dark-theme-control",
+        ),
+        html.Br(),
+        daq.StopButton(id="darktheme-daq-stopbutton", className="dark-theme-control"),
+        html.Br(),
+        daq.Slider(
+            min=0,
+            max=100,
+            value=30,
+            targets={"25": {"label": "TARGET"}},
+            color=theme["primary"],
+            id="darktheme-daq-slider",
+            className="dark-theme-control",
+        ),
+        html.Br(),
+        daq.Tank(
+            min=0,
+            max=10,
+            value=5,
+            id="darktheme-daq-tank",
+            className="dark-theme-control",
+        ),
+        html.Br(),
+        daq.Thermometer(
+            min=95,
+            max=105,
+            value=98.6,
+            id="darktheme-daq-thermometer",
+            className="dark-theme-control",
+        ),
+        html.Br(),
+    ]
+)
 
 app.layout = html.Div(
-    [
+    id="dark-theme-container",
+    children=[
+        daq.ToggleSwitch(id="toggle-theme", label=["Light", "Dark"], value=True),
+        html.Br(),
         html.Div(
-            [
-                html.Div(
-                    [
-                        dcc.Dropdown(
-                            df["Indicator Name"].unique(),
-                            "Fertility rate, total (births per woman)",
-                            id="xaxis-column",
-                        ),
-                        dcc.RadioItems(
-                            ["Linear", "Log"], "Linear", id="xaxis-type", inline=True
-                        ),
-                    ],
-                    style={"width": "48%", "display": "inline-block"},
+            id="theme-colors",
+            children=[
+                daq.ColorPicker(
+                    id="primary-color", label="Primary color", value=dict(hex="#00EA64")
                 ),
-                html.Div(
-                    [
-                        dcc.Dropdown(
-                            df["Indicator Name"].unique(),
-                            "Life expectancy at birth, total (years)",
-                            id="yaxis-column",
-                        ),
-                        dcc.RadioItems(
-                            ["Linear", "Log"], "Linear", id="yaxis-type", inline=True
-                        ),
-                    ],
-                    style={"width": "48%", "float": "right", "display": "inline-block"},
+                daq.ColorPicker(
+                    id="secondary-color",
+                    label="Accent color",
+                    value=dict(hex="#6E6E6E"),
                 ),
-            ]
+                daq.ColorPicker(
+                    id="detail-color", label="Detail color", value=dict(hex="#007439")
+                ),
+            ],
         ),
-        dcc.Graph(id="indicator-graphic"),
-        dcc.Slider(
-            df["Year"].min(),
-            df["Year"].max(),
-            step=None,
-            id="year--slider",
-            value=df["Year"].max(),
-            marks={str(year): str(year) for year in df["Year"].unique()},
+        html.Div(
+            id="dark-theme-components-1",
+            children=[daq.DarkThemeProvider(theme=theme, children=rootLayout)],
+            style={
+                "border": "solid 1px #A2B1C6",
+                "border-radius": "5px",
+                "padding": "50px",
+                "margin-top": "20px",
+            },
         ),
-    ]
+    ],
+    style={"padding": "50px"},
 )
 
 
 @app.callback(
-    Output("indicator-graphic", "figure"),
-    Input("xaxis-column", "value"),
-    Input("yaxis-column", "value"),
-    Input("xaxis-type", "value"),
-    Input("yaxis-type", "value"),
-    Input("year--slider", "value"),
+    Output("dark-theme-components-1", "children"),
+    Input("toggle-theme", "value"),
+    Input("primary-color", "value"),
+    Input("secondary-color", "value"),
+    Input("detail-color", "value"),
 )
-def update_graph(
-    xaxis_column_name, yaxis_column_name, xaxis_type, yaxis_type, year_value
-):
-    dff = df[df["Year"] == year_value]
+def edit_theme(dark, p, s, d):
 
-    fig = px.scatter(
-        x=dff[dff["Indicator Name"] == xaxis_column_name]["Value"],
-        y=dff[dff["Indicator Name"] == yaxis_column_name]["Value"],
-        hover_name=dff[dff["Indicator Name"] == yaxis_column_name]["Country Name"],
-    )
+    if dark:
+        theme.update(dark=True)
+    else:
+        theme.update(dark=False)
 
-    fig.update_layout(margin={"l": 40, "b": 40, "t": 10, "r": 0}, hovermode="closest")
+    if p is not None:
+        theme.update(primary=p["hex"])
+        for child in getattr(rootLayout, "children"):
+            if hasattr(child, "color"):
+                setattr(child, "color", p["hex"])
 
-    fig.update_xaxes(
-        title=xaxis_column_name, type="linear" if xaxis_type == "Linear" else "log"
-    )
+    if s is not None:
+        theme.update(secondary=s["hex"])
+    if d is not None:
+        theme.update(detail=d["hex"])
+    return daq.DarkThemeProvider(theme=theme, children=rootLayout)
 
-    fig.update_yaxes(
-        title=yaxis_column_name, type="linear" if yaxis_type == "Linear" else "log"
-    )
 
-    return fig
+@app.callback(
+    Output("dark-theme-components-1", "style"),
+    Input("toggle-theme", "value"),
+    State("dark-theme-components-1", "style"),
+)
+def switch_bg(dark, currentStyle):
+    if dark:
+        currentStyle.update(backgroundColor="#303030")
+    else:
+        currentStyle.update(backgroundColor="white")
+    return currentStyle
 
 
 if __name__ == "__main__":
